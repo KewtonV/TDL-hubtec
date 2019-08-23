8d36738464d05333abe9655b11633c43cd5d0053
seu nome <kewton.v@gmail.com>
2019-08-23 11:17:05 -0300

    seed,rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-# Criando alguns produtos de exemplo
-    List.create description: "Acordar 05:00AM e fazer café e merendar.", completed: true
-    List.create description: "Às 06:00AM correr 40 min.", completed: true
-    List.create description: "Às 07:00AM ir trabalhar.", completed: true
-    List.create description: "Às 08:00PM ir malhar.", completed: false
-    List.create description: "ÀS 09:00PM jantar.", completed: false
-    List.create description: "ÀS 10:00PM dormir.", completed: false
+    Usuario.create email: 'hubtec@admin.com', password: 123456, password_confirmation: 123456
